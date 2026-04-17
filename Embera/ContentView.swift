c21fe00c089a6f422cae73688ad7b95507c5bc6c
Anisha Dsouza <anishadsouza2000@gmail.com>
2026-04-17 18:55:59 +1000

    Initial Commit

---
+++ Embera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Embera
+//
+//  Created by Anisha Dsouza on 17/4/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Embera/EmberaApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmberaApp.swift
+//  Embera
+//
+//  Created by Anisha Dsouza on 17/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct EmberaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
